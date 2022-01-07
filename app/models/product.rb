e888d8d450e1a2af6390fda90567d7df4dcb45be
PetarSabolek <psabolek1705@gmail.com>
2022-01-07 13:36:02 +0100

    Gemfile update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :description, :title, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif,png,hpg)\z}i,
+    message: "Must be a valid url fot jpg, gif and png"
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.1}
 end
