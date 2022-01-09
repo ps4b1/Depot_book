26828a00fe4cef45524f7e48e23cb0ed5575db92
PetarSabolek <psabolek1705@gmail.com>
2022-01-09 19:30:30 +0100

    Created price and url test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   validates :description, :title, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif,png,hpg)\z}i,
-    message: "Must be a valid url fot jpg, gif and png"
+    with: %r{\.(gif|png|jpg)\z}i,
+    message: "Must be a valid url for jpg, gif and png format"
   }
-  validates :price, numericality: { greater_than_or_equal_to: 0.1}
+  validates :price, numericality: { greater_than_or_equal_to: 0.01}
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product=Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "my Book title", description: "yyy", image_url: "iphone.png")
+
+    product.price =1
+    assert product.valid?
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+  end
+
+  def new_product(image_url)
+    Product.new(title: products(:one).title, description: "xxx", price: 1, image_url: image_url)
+  end
+
+
+  test "image url" do
+    ok= %w{fred.jpg fred.png fred.gif FRED.JPG FRED.Jpg http://a/b/c/fred.gif}
+    bad=%w{fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be valid"
+    end
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
+  end
 end
