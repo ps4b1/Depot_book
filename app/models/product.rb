00dd5a1ce372a83ea02e2d6bce02d34410616deb
PetarSabolek <psabolek1705@gmail.com>
2022-01-15 13:57:20 +0100

    Created line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :description, :title, :image_url, presence: true
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
@@ -6,4 +8,14 @@ class Product < ApplicationRecord
     message: "Must be a valid url for jpg, gif and png format"
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
+
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
