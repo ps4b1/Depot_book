7064e6b25ec03074112b3838bdb466283bc1d342
PetarSabolek <psabolek1705@gmail.com>
2022-02-18 00:41:37 +0100

    Set up mails to be sent

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -8,6 +8,7 @@ class SupportRequestsController < ApplicationController
   def update
     support_request = SupportRequest.find(params[:id])
     support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.respond(support_request).deliver_now
     redirect_to support_requests_url
   end
 end
